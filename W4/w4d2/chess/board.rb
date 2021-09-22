5eac678bda9a2959fb3844cfde93e9b92c91dbca
shuang zheng <shuangzheng16@gmail.com>
2021-09-22 10:06:29 -0700

    piece class / board class updates -sz

--- W4/w4d2/chess/board.rb
+++ W4/w4d2/chess/board.rb
@@ -1,55 +1,72 @@
 require_relative "piece"
 
 class Board
+  attr_accessor :grid
 
   def initialize
-    @board = Array.new(8) {Array.new(8)}
-    @piece = Piece.new
+    @grid = Array.new(8) {Array.new(8)}{ NullPiece.instance}}    
+    @piece = Piece.new #current pos 
   end
 
+  # i = 0 , 1  => row 1, row 2, 
+             # => row 7, row 8, (length -1 -i)=6 , (length-1-i)
+  def self.initial_populate
+    i = 0
+    while i < 2
+      @grid[i]=@grid[i].map {|ele| ele = Piece.new}
+      @grid[7-i] = @grid[7-i].map {|ele| ele = Piece.new}
+      i += 1
+    end
+  end
+  
   def [](pos)
-    @board[pos[0][pos[1]]
+    row = pos[0]
+    col = pos[1]
+    @grid[row][col] 
   end
 
   def []=(pos, val)
-    @board[pos[0][pos[1]] = val
+    row = pos[0]
+    col = pos[1]
+    @grid[row][col]= val
   end
 
-  def valid_pos?(pos)
-    @piece.valid_move.include?(pos)
-  end
 
-  def add_piece(piece, pos)
-    @board[pos]= piece
-  end
+  # def valid_pos?(pos)
+  #   @piece.valid_move.include?(pos)
+  # end
 
-  def find_king(color)
-    (0...@board.length).each do |i|
-      (0...@board.length).each do |j|
-        if @board[i][j].color == :♔
-          return [i,j] 
-        end
-      end
-    end
-    return nil   
-  end
+  # def add_piece(piece, pos)
+  #   @board[pos]= piece
+  # end
 
-  def checkmate?(color)
-    #check if WHITE/BLACK king
-    #check if king has valid moves available
-    # king.valid_moves.include?(xy)
-    king_position = find_king(color)
-    @board[king_position].valid_moves.empty? && in_check?(color)
-  end
+  # def find_king(color)
+  #   (0...@board.length).each do |i|
+  #     (0...@board.length).each do |j|
+  #       if @board[i][j].color == :♔
+  #         return [i,j] 
+  #       end
+  #     end
+  #   end
+  #   return nil   
+  # end
 
-  def in_check?(color) 
-    #when have no valid moves, wnt to check, if piece is in-chek?
-    #king has valid moves && another piece wants to eat king
-  end
+  # def checkmate?(color)
+  #   #check if WHITE/BLACK king
+  #   #check if king has valid moves available
+  #   # king.valid_moves.include?(xy)
+  #   king_position = find_king(color)
+  #   @board[king_position].valid_moves.empty? && in_check?(color)
+  # end
 
-  
+  # def in_check?(color) 
+  #   #when have no valid moves, wnt to check, if piece is in-chek?
+  #   #king has valid moves && another piece wants to eat king
+  # end
 
-    
-
-
+  #def move_
+ 
 end
+
+new_board = Board.new
+p new_board
--- W4/w4d2/chess/piece.rb
+++ W4/w4d2/chess/piece.rb
@@ -1,7 +1,29 @@
+require_relative "board"
+
 class Piece
 
   # @queen = Queen.new
   #   @rook = Rook.new
   #   @knight = Knight.new
+  def initialize(color, board, pos)
+
+    @color = color
+    @board = board  # piece = Piece.new(b, ) #new_board = Board.new
+    @pos = pos #yellow 
+
+  end
+
 
 end
+
+class NullPiece < Piece
+  def initialize("red", board, )
+    super
+
+  end
+end
+
+# new_board = Board.new
+# piece_1 = Piece.new("black", new_board, [1,2])
+# piece_2 = Piece.new("black", new_board, [0,2])
+# null_piece = NullPiece("red", new_board, [1,1])
