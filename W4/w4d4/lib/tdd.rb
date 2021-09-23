ca548952653dba297dcbad317207e95fa3011851
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-09-23 14:18:14 -0700

    two_sum, transpose. Tested and working

--- W4/w4d4/lib/tdd.rb
+++ W4/w4d4/lib/tdd.rb
@@ -4,4 +4,20 @@ class Array
   def my_uniq
     self.uniq
   end
+
+  def two_sum 
+    new_arr = []
+    self.each.with_index do |num1, idx1|
+      self.each.with_index do |num2, idx2|
+        if idx2 > idx1 and num1 + num2 == 0
+          new_arr << [idx1, idx2]
+        end
+      end
+    end
+    new_arr
+  end
+
+  def my_transpose
+    self.transpose
+  end
 end
--- W4/w4d4/spec/tdd_spec.rb
+++ W4/w4d4/spec/tdd_spec.rb
@@ -1,17 +1,43 @@
 require 'tdd.rb'
 
 describe Array do
-  subject(:array) { [1,2,5,6,2] }
+  subject(:array) { [-1, 0, 2, -2, 1] }
   subject(:array2) { [1,2,3,4,5] }
 
   describe "#my_uniq" do
     it "should remove duplicate elements" do
-      expect(array.my_uniq.length).to eq(4)
+      expect(array.my_uniq.length).to eq(array.uniq.length)
     end
     it "should't remove other elements" do
-      expect(array2.my_uniq.length).to eq(5)
+      expect(array2.my_uniq.length).to eq(array2.uniq.length)
+    end
+  end
+
+  describe "#two_sum" do 
+    it "should find all pairs of position where the elements sum to zero" do
+      expect(array.two_sum).to eq([[0,4],[2,3]])
+    end
+
+    it "should display the output in order of ascending indicies" do
+      expect(array.two_sum).to eq(array.two_sum.sort)
+    end
+  end
+
+  describe "#my_transpose" do
+    subject(:darray) { [[0, 1, 2],[3, 4, 5],[6, 7, 8]] }
+    subject(:messed_up_array) { [["wolf"],3, 4, 5, [[[6]]], 7] }
+
+    it "takes in a 2d array" do
+      expect{messed_up_array.my_transpose}.to raise_error(TypeError)
+    end
+
+    it "should transpose a 2d array" do
+      expect(darray.my_transpose).to eq(darray.transpose)
     end
 
   end
+
+  
+
 
 end
