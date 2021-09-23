cad5cc62ff7e0b176ca3ff10002ead5320bf45c7
kenny <kcheng16@gmail.com>
2021-09-22 17:31:28 -0700

    complete pawn forward_steps, forward_dir, at_start_row?

--- W4/w4d2/chess/pawn.rb
+++ W4/w4d2/chess/pawn.rb
@@ -8,17 +8,52 @@ class Pawn < Piece
     @symbol = :P
   end
 
-  def move_dirs
-    #initialization : pawn white at row 1, black at row 6
-    if color == "W"
-      return [1+pos[0],0]
-    elsif color == "B"
-      return [pos[0]-1,0]
-    else 
-      raise "pawn move_dirs errors" #error purpose
+  def moves 
+    pawn_moves = []                     
+    pawn_moves << [self.forward_dir + pos[0], pos[1]]
+    if self.forward_steps == 2
+      pawn_moves << [(self.forward_dir * 2) + pos[0], pos[1]]
     end
   end 
 
-  #
+  private
+
+  def at_start_row?
+    if pos[0] == 1 || pos[0] == 6
+      return true
+    else
+      return false
+    end
+  end
+
+  def forward_dir #returns 1 or -1    
+    if color == "W" 
+      return 1
+    elsif
+      color == "B"
+      return -1
+    else
+      raise "error"
+    end
+  end
+
+  def forward_steps   #pawn.forward_steps
+    if self.at_start_row?
+      return 2
+    else
+      return 1
+    end
+
+  end
+
+  def side_attacks
+    #if object at diagonal, is a different color, take its place
+    #black or white
+    #check if within board + opposite color + if its a piece (not null pc)
+    #going up:[-1, 1][1, 1]
+    #going down: [-1, 1*-1] , [1, 1*-1]
+    
+
+  end
 
 end
