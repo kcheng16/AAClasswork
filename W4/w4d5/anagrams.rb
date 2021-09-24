ca7a59845865e59eb2ad87384a9f37e7b72e23a9
kenny <kcheng16@gmail.com>
2021-09-24 16:03:28 -0700

    start anagrams; defined phase 1

---
+++ W4/w4d5/anagrams.rb
@@ -0,0 +1,9 @@
+#phase 1: O(n!)
+# def first_anagram?(string1, string2) #
+#   perm = string1.split("").permutation.to_a
+#   perm.map!(&:join)
+#   perm.include?(string2)
+# end
+
+# p first_anagram?("hel", "hello")
+
