2e46e332efaca0ed4d238413d94277214a390bc6
kenny <kcheng16@gmail.com>
2021-09-24 19:16:48 -0700

    added linear approach to okay_two_sum?

--- W4/w4d5/two_sums.rb
+++ W4/w4d5/two_sums.rb
@@ -18,19 +18,30 @@ end
 def okay_two_sum?(arr, target)
   sorted = arr.sort
 
-  sorted.each.with_index do |el1, i1|
-    sorted.each.with_index do |el2, i2|
-      if i2 > i1 && el1 + el2 == target
-        return true
-      end
+  i = 0 
+  j = sorted.length - 1 
+
+  while i != j
+    if sorted[i] + sorted[j] == target
+      return true
+    elsif sorted[i] + sorted[j] > target
+      j -= 1
+    else
+      i += 1
     end
   end
   false
+  # sorted.each.with_index do |el1, i1|
+  #   sorted.each.with_index do |el2, i2|
+  #     if i2 > i1 && el1 + el2 == target
+  #       return true
+  #     end
+  #   end
+  # end
+  # false
 end
 
 arr = [0, 1, 5, 7]
 p okay_two_sum?(arr, 6) # => should be true
 p okay_two_sum?(arr, 10) # => should be false
 
-Benchmark.bm do |arr| 
-  okay_two_sum?
