81d949a92bacfa52a7c4453cb7afdc741678bccb
alexlolas <alexanderlolas@yahoo.com>
2021-09-28 17:30:53 -0700

    Finished Part 4 Project

--- W5/w5d2/SQLZoo/skeleton/lib/04_select_within_select.rb
+++ W5/w5d2/SQLZoo/skeleton/lib/04_select_within_select.rb
@@ -35,6 +35,9 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+  SELECT name
+  FROM countries
+  WHERE population > 141500000;
   SQL
 end
 
@@ -42,6 +45,13 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+  SELECT name
+  FROM countries 
+  WHERE continent = 'Europe' AND (gdp/population) > 
+        ( SELECT (gdp/population) AS gdp_per_capita
+          FROM countries
+          WHERE name = 'United Kingdom'
+  );
   SQL
 end
 
@@ -49,6 +59,17 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  SELECT name, continent 
+  FROM countries
+  WHERE continent = ( SELECT continent
+          FROM countries
+  
+          WHERE (name = 'Belize' AND continent = 'Americas')
+           
+  )
+       OR continent = (SELECT continent
+        FROM countries
+        WHERE name = 'Belgium' AND continent = 'Europe');
   SQL
 end
 
@@ -56,6 +77,17 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+   SELECT name, population 
+  FROM countries
+  WHERE population < ( SELECT population
+          FROM countries
+  
+          WHERE (name = 'Poland')
+           
+  )
+       AND population > (SELECT population
+        FROM countries
+        WHERE name = 'Canada');
   SQL
 end
 
@@ -65,5 +97,13 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  
+  SELECT name, continent, population
+  FROM countries
+  WHERE continent IN (SELECT continent
+  FROM countries
+  GROUP BY continent 
+  HAVING MAX(population) < 25000000);
+  
   SQL
 end
