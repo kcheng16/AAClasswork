04381a643f3b256d5c7c9701937bfb42950eccf7
marksterr <mrkdawes8@gmail.com>
2021-09-29 14:21:06 -0400

    create database

--- W5/w5d3/aa_questions/import_db.sql
+++ W5/w5d3/aa_questions/import_db.sql
@@ -1,39 +1,55 @@
+PRAGMA foreign_keys = ON;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_likes;
+
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   fname VARCHAR(255) NOT NULL,
-  lname VARCHAR(255) NOT NULL,
-
+  lname VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE questions (
-  id INTEGER PRIMARY KEY
+  id INTEGER PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   body TEXT NOT NULL,
-  author.id INTEGER NOT NULL,
+  author_id INTEGER NOT NULL,
 
-  FOREIGN KEY (author.id) REFERENCES users(id)
-)
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
 
 CREATE TABLE question_follows (
-  id INTEGER PRIMARY KEY,
+  id INTEGER PRIMARY KEY
 
-)
+);
 
 CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
   question_id INTEGER NOT NULL,
   parent_reply_id INTEGER,
-  aurthor_id INTEGER NOT NULL,
+  author_id INTEGER NOT NULL,
   body TEXT NOT NULL,
 
-  FOREIGN KEY (questions_id) REFERENCES questions(id)
-  FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
   FOREIGN KEY (author_id) REFERENCES users(id)
 
 
-)
+);
 
 CREATE TABLE question_likes (
   id INTEGER PRIMARY KEY,
+  author_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  likes INTEGER NOT NULL,
 
-)
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ("Kenny", "Cheng"), ("John", "Smith");
