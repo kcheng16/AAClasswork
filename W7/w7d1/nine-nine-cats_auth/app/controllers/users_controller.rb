8eff09ff69d809ded643c1266848ed02a9d64681
mattpettenato <mattpettenato@gmail.com>
2021-10-11 15:10:27 -0700

    Sessions

---
+++ W7/w7d1/nine-nine-cats_auth/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  
+
+  def new
+    @user = User.new
+    render :new
+  end
+  
+  def create
+    user = User.new(user_params)
+    if user.save
+      #login(user) 
+      redirect_to user_url(user)
+    else
+      render json: user.error.full_messages, status: 422
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username)
+  end
+end
