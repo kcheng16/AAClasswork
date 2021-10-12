89380fadd878559b22d3f8375571f35f189c916b
kenny <kcheng16@gmail.com>
2021-10-12 15:34:10 -0700

    Complete Bands controller

--- W7/w7d2/musicapp/app/controllers/bands_controller.rb
+++ W7/w7d2/musicapp/app/controllers/bands_controller.rb
@@ -6,8 +6,12 @@ class BandsController < ApplicationController
 
   def show
     @band = Band.find(params[:id])
-
     render :show
+  end
+
+  def new
+    @band = Band.new
+    render :new
   end
 
   def create
@@ -21,5 +25,35 @@ class BandsController < ApplicationController
     end
   end
 
+  def edit
+    @band = Band.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @band = Band.find(params[:id])
+
+    if band.update
+      redirect_to band_url(@band)
+    else
+      flash.now[:error] = @band.errors.full_messages
+    end
+  end
+
+  def destroy
+    @band = Band.find(params[:id])
+
+    if @band.destroy
+      flash[:message] = ["Band delete success"]
+      redirect_to users_url
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :destroy
+    end
+  end
   
+
+  def band_params
+    params.require(:band).permit(:name)  
+  end
 end
