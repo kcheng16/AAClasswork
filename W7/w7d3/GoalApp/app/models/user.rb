e89e2316a05daaf6fb63f9db0e24baab03296bf2
Abraham Fong <abisfong@ucsc.edu>
2021-10-13 17:00:00 -0700

    Creaet is_valid_password? and fix validation bug

--- W7/w7d3/GoalApp/app/models/user.rb
+++ W7/w7d3/GoalApp/app/models/user.rb
@@ -15,17 +15,26 @@
 #  index_users_on_username       (username) UNIQUE
 #
 class User < ApplicationRecord
-  before_validation :ensure_session_token
+  after_initialize :ensure_session_token
   
   validates :username, :password_digest, :session_token, presence: true
-  validates :username, uniqueness: true
+  validates :username, :session_token, uniqueness: true
   validates :password, length: {minimum: 6}, allow_nil: true
-  
+
   attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username)
+  end
 
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_valid_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
   end
 
   def ensure_session_token
--- W7/w7d3/GoalApp/spec/models/user_spec.rb
+++ W7/w7d3/GoalApp/spec/models/user_spec.rb
@@ -25,9 +25,9 @@ RSpec.describe User, type: :model do
     it {should validate_length_of(:password).is_at_least(6)}
     
     describe "uniqueness" do
-      # before :each do
-      #   create(:user)
-      # end
+      before :each do
+        create(:user)
+      end
 
       it {should validate_uniqueness_of(:username)}
       it {should validate_uniqueness_of(:session_token)}
