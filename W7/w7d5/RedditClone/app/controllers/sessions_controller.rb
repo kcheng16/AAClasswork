416785206d1f6d5980a31ff193ba97de2d5ee917
Grayson Poon <grayson.a.poon@gmail.com>
2021-10-15 14:45:15 -0700

    Write sessions controller and application controller

--- W7/w7d5/RedditClone/app/controllers/sessions_controller.rb
+++ W7/w7d5/RedditClone/app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by_credentials(username: params[:user][:username], password: params[:user][:password])
+
+    if user
+      login(user)
+      redirect_to user_url(user)
+    else
+      flash.now[:errors] = ["Invalid username or password"]
+      render :new
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout
+      redirect_to new_session_url
+    else
+      redirect_to new_session_url
+    end
+  end
+
 end
