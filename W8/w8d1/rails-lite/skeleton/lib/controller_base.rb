d2b05091dd73a27aa41438d885934a2fb3d834f6
kenny <kcheng16@gmail.com>
2021-10-18 17:10:59 -0700

    complete phase 1

--- W8/w8d1/rails-lite/skeleton/lib/controller_base.rb
+++ W8/w8d1/rails-lite/skeleton/lib/controller_base.rb
@@ -4,24 +4,34 @@ require 'erb'
 require_relative './session'
 
 class ControllerBase
-  attr_reader :req, :res, :params
+  attr_reader :req, :res, :params, :already_built_response
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, env)
+    @req = Rack::Request.new(env)
+    @res = Rack::Response.new
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+      @already_built_response = true
+      res.status = 302
+      res.location = url
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    @already_built_response = true
+    @res.write(content)
+    @res['Content-Type'] = content_type 
+    nil
   end
 
   # use ERB and binding to evaluate templates
