830b4561f3ba28d4e30a4cc82bf931da43c2ea5d
nraff <nraff510@gmail.com>
2021-10-19 17:16:55 -0700

    add myMap to Array prototype

--- W8/w8d2/intro_to_js/phase_1_arrays.js
+++ W8/w8d2/intro_to_js/phase_1_arrays.js
@@ -26,54 +26,6 @@ Array.prototype.twoSum = function (parameters) {
   return answer
 }
 
-const twoSum = function (arr) {
-  let answer = [];
-  for (let i = 0; i < arr.length; i++) {
-    for (let x = i + 1; x < arr.length; x++) {
-      if (arr[i] + arr[x] === 0) {
-        answer.push([i, x])
-      }
-    }
-  }
-  return answer
-}
-
-// [1,2,3,4,5]
-// [2,3,4,5]
-
-// const twoSumEach = function (arr) {
-//   let answer = [];
-//   arr.forEach((topEle, index) => {
-//     arr.slice(index+1, arr.length).forEach( (subEle, subIndex) => {
-//       if (topEle + subEle === 0) {
-//         answer.push([index, subIndex]);
-//       }
-//     })
-//   })
-//   return answer;
-// }
-
-// const f1 = function (arr) {
-//   let newArr = []
-//   // for(let i = 0; i < this.length; i++){
-//     arr.forEach(ele => {
-    
-//     if (!newArr.includes(ele)) {
-//       newArr.push(ele);
-
-//     }
-//   })
-//   return newArr;
-// }
-
-
-// console.log(f1([1,2,3,1,2,3]))
-//  [ [1,2,3],
-//    [a,b,c]  ]
-// [0...array.length].each do |index1|
-//   subarray = []
-//  [0...array.length].each do |index2|
-
 Array.prototype.transposed = function () {
   let transposed = [];
     for (let colidx = 0; colidx <= this.length; colidx++){
---
+++ W8/w8d2/intro_to_js/phase_2_enumerable.js
@@ -0,0 +1,18 @@
+Array.prototype.myEach = function (paramCallback) {
+  for (i = 0; i < this.length; i++) {
+    paramCallback(this[i]);
+  }
+}
+
+Array.prototype.myMap = function (paramCallback) {
+  let results = [];
+  this.myEach(ele => {
+    results.push(paramCallback(ele));
+  });
+  return results;
+}
+
+const callback = function (ele) {
+  console.log(`${ele}!`);
+  return `${ele}!`;
+}
