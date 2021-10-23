95ae502e7ff60ee526afb0800487d5918915ff36
Julian Erville <jerville@gmail.com>
2021-10-22 20:06:54 -0400

    Maybe finish curry monkey patch, maybe not

--- W8/w8d5/curriedsum.js
+++ W8/w8d5/curriedsum.js
@@ -1,11 +1,34 @@
-function curriedSum (number_of_ingredients){
-  let pot = 0;
-  return function addIngredients(ingredients){
-    pot += ingredients;
-    number_of_ingredients -= 1;
-  if (number_of_ingredients === 0){
-    return pot;
+// function curriedSum (number_of_ingredients){
+//   let pot = 0;
+//   return function addIngredients(ingredients){
+//     pot += ingredients;
+//     number_of_ingredients -= 1;
+//   if (number_of_ingredients === 0){
+//     return pot;
+//   } else {
+//     return addIngredients //keep adding ingredients
+//   }
+//   }
+// }
+
+Function.prototype.curry = function(numberOfIngredients) {
+  let pot = [];
+
+  let that = this;
+
+  return function addIngredients(...ingredients){
+    
+    
+  if (numberOfIngredients === pot.length){
+    console.log(that);
+    return that.apply(pot);
   } else {
+    for (ingredient of ingredients) {
+      if (numberOfIngredients === pot.length){
+        return pot;
+      }
+      pot.push(ingredient);
+    }
     return addIngredients //keep adding ingredients
   }
   }
@@ -13,7 +36,9 @@ function curriedSum (number_of_ingredients){
 
 
 
+// Function.prototype.apply = function(context) {
 
+// }
 
-const sum = curriedSum(4);
-console.log(sum(5)(30)(20)(1)); // => 56
+// const sum = curriedSum(4);
+// console.log(sum(5)(30)(20)(1)); // => 56
