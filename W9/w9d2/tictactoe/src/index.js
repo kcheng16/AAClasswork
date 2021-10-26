f786988a931fd97bd58e20416bf44738350cb24e
kenny <kcheng16@gmail.com>
2021-10-26 16:48:55 -0700

    complete setupBoard

--- W9/w9d2/tictactoe/src/index.js
+++ W9/w9d2/tictactoe/src/index.js
@@ -1,13 +1,13 @@
 const View = require("./ttt-view.js");
 const Game = require("../ttt_node/game");
 
-const game1 = new Game();
 
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
   const board = document.getElementsByClassName("ttt");
+  const game1 = new Game();
   const view1 = new View(game1, board);
-
+  console.log(view1);
 });
 
--- W9/w9d2/tictactoe/src/ttt-view.js
+++ W9/w9d2/tictactoe/src/ttt-view.js
@@ -1,10 +1,35 @@
 class View {
   constructor(game, el) {
     this.game = game;
-    this.el = el
+    
+    this.setupBoard();
+    const ul = document.querySelector("ul")
+    this.el = ul
   }
 
-  setupBoard() {}
+  setupBoard() {
+    //create ul & li
+    const classtictactoe = document.querySelector(".ttt");
+    console.log(classtictactoe);
+    const ul = document.createElement("UL");
+              // let text = document.createTextNode("water");
+              // ul.appendChild(text);
+    classtictactoe.appendChild(ul);
+  
+    // add attribute to li (data-pos) by using game.board @ position
+    for (let i = 0; i < 9; i++) {
+      let li = document.createElement("li");
+      ul.appendChild(li);
+      let x = 0;
+      if (i >= 3 && i < 6) x = 1;
+      if (i >= 6) x = 2;
+      
+      let y = i % 3;
+      li.setAttribute("data-pos", [`${x}`,`${y}`]);//[0,0] [0,1] [0,2]
+                            // <li data-pos="[x, y]"></li>
+    }
+
+  }
   
   bindEvents() {}
 
