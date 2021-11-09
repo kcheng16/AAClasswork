d37c82f2a49f8a39eee7ef305f10864878d0e1a1
kenny <kcheng16@gmail.com>
2021-11-09 14:23:49 -0800

    start Todos proj

---
+++ W11/Todos-pt1/frontend/actions/todo_actions.js
@@ -0,0 +1,2 @@
+export const RECEIVE_TODOS = "RECEIVE_TODOS";
+export const RECEIVE_TODO = "RECEIVE_TODO";
---
+++ W11/Todos-pt1/frontend/todo_redux.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Content from './content'
+
+document.addEventListener('DOMContentLoaded', () => {
+  let root = document.getElementById('root')
+  ReactDOM.render(<Content />, root)
+})
+
