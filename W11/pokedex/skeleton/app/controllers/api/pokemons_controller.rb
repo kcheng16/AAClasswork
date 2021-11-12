7943bb943d575558df900fdc473067405f65e4d4
kenny <kcheng16@gmail.com>
2021-11-12 11:15:39 -0800

    added pokemon controller + jbuilder view

---
+++ W11/pokedex/skeleton/app/controllers/api/pokemons_controller.rb
@@ -0,0 +1,12 @@
+class Api::PokemonsController < ApplicationController
+  def index
+    @pokemons = Pokemon.all
+    render :index
+  end
+
+  def show
+    @pokemon = Pokemon.find(params[:id])
+
+    # render json: @pokemon
+  end
+end
